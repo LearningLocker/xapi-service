9390e666379c7efcb3cc352751e36ece7dfdd817
Ryan Smith <0ryansmith1994@gmail.com>
2017-10-25 15:50:22 +0100

    style(formatting): Formats test script logs in Travis.

--- test.sh
+++ test.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env sh
-echo "Running conformance tests."
+log () {
+  echo "- XAPI SERVICE LOG: $1"
+}
 
-echo "Starting the server on port $EXPRESS_PORT."
+log "Running conformance tests."
+
+log "Starting the server on port $EXPRESS_PORT."
 npm start &
 
-echo "Removing clients and creating a new client."
+log "Removing clients and creating a new client."
 mongo learninglocker_v2 --eval 'db.client.remove({})'
 mongo learninglocker_v2 --eval 'db.client.insert({"createdAt":ISODate("2017-10-25T14:39:44.962Z"),"updatedAt":ISODate("2017-10-25T14:39:58.376Z"),"organisation":ObjectId("58fe13e34effd3c26a7fc4b6"),"lrs_id":ObjectId("5901bc9c81a4a731c2dec4f0"),"title":"Conformance Tests","scopes":["xapi/all","all"],"isTrusted":true,"authority":"{\"objectType\":\"Agent\",\"name\":\"New Client\",\"mbox\":\"mailto:hello@learninglocker.net\"}","api":{"basic_secret":"BBB","basic_key":"AAA"}})'
 
-echo "Installing and running the ADL conformance test suite."
+log "Installing and running the ADL conformance test suite."
 npm i --no-save git+https://ryansmith94@github.com/ryansmith94/lrs-conformance-test-suite#patch-1
 node ./node_modules/lrs-conformance-tests/bin/console_runner.js -e "http://localhost:$EXPRESS_PORT/data/xAPI" -a -u "AAA" -p "BBB"
 
-echo "Stopping the server."
-ps aux | grep [p]hp | awk '{print $2}' | xargs kill
+log "Stopping the server."
+ps aux | grep [n]ode\ dist\/server | awk '{print $2}' | xargs kill
 
-echo "Completed conformance tests."
+log "Completed conformance tests."
