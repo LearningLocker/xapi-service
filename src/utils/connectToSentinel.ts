c6ae0426278f1188185d4dad20cf434b77d838de
Ryan Smith <3045513+ryansmith94@users.noreply.github.com>
2018-03-01 13:26:39 +0000

    fix(sentinel): Adds support for password and database. (#75)

--- src/utils/connectToSentinel.ts
+++ src/utils/connectToSentinel.ts
@@ -7,7 +7,9 @@ export default once((): () => Promise<Ioredis.Redis> => {
   return once(async () => {
     logger.info('Creating sentinel connection');
     return new Ioredis({
+      db: defaultTo(config.sentinel.db, 0),
       name: defaultTo(config.sentinel.name, 'mymaster'),
+      password: config.sentinel.password,
       sentinels: defaultTo(config.sentinel.sentinels, [{ host: '127.0.0.1', port: 6379 }]),
     });
   });
