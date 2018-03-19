57cc8e97bb8c75344e86c7990268c8df28a88ad5
Ryan Smith <3045513+ryansmith94@users.noreply.github.com>
2018-03-19 15:30:30 +0000

    fix(deps): Updates xapi dependencies. (#78)

--- src/conformanceServer.ts
+++ src/conformanceServer.ts
@@ -3,6 +3,21 @@ import './server'; // tslint:disable-line:no-import-side-effect
 import { ObjectID } from 'mongodb'; // tslint:disable-line:ordered-imports
 import connectToMongoDb from './utils/connectToMongoDb';
 
+const testOrg = {
+  _id: new ObjectID('5988f0f00000000000000000'),
+  createdAt: new Date('2017-10-25T14:39:44.962Z'),
+  name: 'Test Org',
+  updatedAt: new Date('2017-10-25T14:39:58.376Z'),
+};
+const testStore = {
+  _id: new ObjectID('5988f0f00000000000000001'),
+  createdAt: new Date('2017-10-25T14:39:44.962Z'),
+  description: 'Test LRS Description',
+  organisation: testOrg._id,
+  statementCount: 0,
+  title: 'Test LRS',
+  updatedAt: new Date('2017-10-25T14:39:58.376Z'),
+};
 const testClient = {
   api: {
     basic_key: 'AAA',
@@ -15,20 +30,21 @@ const testClient = {
   }),
   createdAt: new Date('2017-10-25T14:39:44.962Z'),
   isTrusted: true,
-  lrs_id: new ObjectID('5901bc9c81a4a731c2dec4f0'),
-  organisation: new ObjectID('58fe13e34effd3c26a7fc4b6'),
+  lrs_id: testStore._id,
+  organisation: testOrg._id,
   scopes: ['xapi/all', 'all'],
   title: 'Conformance Tests',
   updatedAt: new Date('2017-10-25T14:39:58.376Z'),
 };
 
 (async () => {
-  const db = connectToMongoDb();
-  const collection = (await db()).collection('client');
-  console.log('Removing clients for ADL conformance tests.');
-  await collection.deleteMany({});
-  console.log('Inserting test client for ADL conformance tests.');
-  await collection.insertOne(testClient);
+  const db = await connectToMongoDb()();
+  console.log('Dropping database for ADL conformance tests.');
+  await db.dropDatabase();
+  console.log('Seeding database for ADL conformance tests.');
+  await db.collection('organisation').insertOne(testOrg);
+  await db.collection('lrs').insertOne(testStore);
+  await db.collection('client').insertOne(testClient);
 })().then(() => {
   console.log('Completed seeding for ADL conformance tests.');
 }).catch((err) => {
