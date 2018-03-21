8374474dabd4c8bd701d029142f29f5f0c531155
Ryan Smith <3045513+ryansmith94@users.noreply.github.com>
2018-03-21 09:34:48 +0000

    fix: Errors for untrusted clients. Includes [activities ^4.1.1](https://github.com/LearningLocker/xapi-activities/releases/tag/v4.1.1), [agents ^4.1.1](https://github.com/LearningLocker/xapi-agents/releases/tag/v4.1.1), [state ^4.1.1](https://github.com/LearningLocker/xapi-state/releases/tag/v4.1.1), [statements ^7.1.1](https://github.com/LearningLocker/xapi-statements/releases/tag/v7.1.1) (#79)

---
+++ scripts/updateDeps.js
@@ -0,0 +1,64 @@
+const git = require('simple-git/promise')();
+const rimraf = require('rimraf');
+const { promisify } = require('util');
+const shelljs = require('shelljs');
+const colors = require('colors');
+const { join } = require('path');
+const package = require(join(process.cwd(), 'package.json'));
+
+const getCommitMessage = () => {
+  const getDepMessage = (dep) => {
+    const versionRange = package.dependencies[`@learninglocker/xapi-${dep}`];
+    const version = versionRange.replace('^', 'v');
+    const releaseLink = `https://github.com/LearningLocker/xapi-${dep}/releases/tag/${version}`;
+    return `[${dep} ${versionRange}](${releaseLink})`;
+  };
+  const semverMessage = process.argv[2];
+  const deps = ['activities', 'agents', 'state', 'statements'];
+  const depsMessage = deps.map(getDepMessage).join(', ');
+  return `${semverMessage} Includes ${depsMessage}`;
+};
+
+const exec = (command) => {
+  return new Promise((resolve, reject) => {
+    console.log(colors.cyan(`Starting: ${command}`));
+    const child = shelljs.exec(command, { async: true }, (code) => {
+      if (code !== 0) {
+        console.log(colors.cyan(`Completed unsuccessfully: ${command}`));
+        reject(new Error());
+        return;
+      }
+      console.log(colors.cyan(`Completed successfully: ${command}`));
+      resolve();
+    });
+  });
+};
+
+const main = async () => {
+  await git.fetch();
+  await git.checkout('master');
+  await git.pull();
+  await git.deleteLocalBranch('xapi-deps');
+  await git.checkoutLocalBranch('xapi-deps');
+  await promisify(rimraf)(join(process.cwd(), 'node_modules'));
+  await exec('yarn install --ignore-engines');
+  await exec('npm i @learninglocker/xapi-activities@latest');
+  await exec('npm i @learninglocker/xapi-agents@latest');
+  await exec('npm i @learninglocker/xapi-state@latest');
+  await exec('npm i @learninglocker/xapi-statements@latest');
+  await exec('yarn add --ignore-engines @learninglocker/xapi-activities@latest');
+  await exec('yarn add --ignore-engines @learninglocker/xapi-agents@latest');
+  await exec('yarn add --ignore-engines @learninglocker/xapi-state@latest');
+  await exec('yarn add --ignore-engines @learninglocker/xapi-statements@latest');
+  await git.add('./*');
+  await git.commit(getCommitMessage());
+  await git.push('origin', 'xapi-deps');
+};
+
+main().then(() => {
+  console.log(colors.green('Completed successfully'));
+}).catch((err) => {
+  console.error(colors.red(err));
+  console.log(colors.red('Completed unsuccessfully'));
+});
+// node scripts/updateDeps.js "fix: message"
