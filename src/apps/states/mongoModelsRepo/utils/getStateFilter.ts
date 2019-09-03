0cb1e36037cefdf178663fbfe75823de7a6c77b1
Ryan Smith <0ryansmith1994@gmail.com>
2019-09-03 17:29:18 +0100

    refactor: Removes unused import to resolve LGTM error.

--- src/apps/states/mongoModelsRepo/utils/getStateFilter.ts
+++ src/apps/states/mongoModelsRepo/utils/getStateFilter.ts
@@ -1,4 +1,3 @@
-import { ObjectID } from 'mongodb'; /* tslint:disable-line:no-unused */
 import Agent from '../../models/Agent';
 import ClientModel from '../../models/ClientModel';
 import getStatesFilter from './getStatesFilter';
