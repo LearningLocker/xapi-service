75c5e8f3a3150b6fb4e31909de29bf64c0a3f516
Vadim Nikolaenko <vadimnikolaenko.biz@gmail.com>
2020-01-04 00:51:50 +0200

    refactor(Interaction activity types): Change `ActivityInteractionType` from `type` to `enum`

--- src/apps/statements/models/ActivityInteractionType.ts
+++ src/apps/statements/models/ActivityInteractionType.ts
@@ -1,13 +1,14 @@
-type ActivityInteractionType =
-  'true-false'
-  | 'choice'
-  | 'fill-in'
-  | 'long-fill-in'
-  | 'matching'
-  | 'performance'
-  | 'sequencing'
-  | 'likert'
-  | 'numeric'
-  | string;
+enum ActivityInteractionType {
+  TRUE_FALSE = 'true-false',
+  CHOICE = 'choice',
+  FILL_IN = 'fill-in',
+  LONG_FILL_IN = 'long-fill-in',
+  MATCHING = 'matching',
+  PERFORMANCE = 'performance',
+  SEQUENCING = 'sequencing',
+  LIKERT = 'likert',
+  NUMERIC = 'numeric',
+  OTHER = 'other',
+}
 
 export default ActivityInteractionType;
