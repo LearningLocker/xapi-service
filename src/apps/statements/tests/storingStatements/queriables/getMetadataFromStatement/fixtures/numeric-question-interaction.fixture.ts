7ca8f705f7982b45cad5dd1ca19736d9b60a42a0
Yevgenii Sharpinskii <yevgenii.sharpinskii@onix-systems.com>
2020-01-14 19:07:24 +0200

    test(getMetadataFromStatement): Add tests related to a numeric question metadata
    Closes LL-255

---
+++ src/apps/statements/tests/storingStatements/queriables/getMetadataFromStatement/fixtures/numeric-question-interaction.fixture.ts
@@ -0,0 +1,27 @@
+import ActivityInteractionType from '../../../../../models/ActivityInteractionType';
+import InteractionActivityDefinition from '../../../../../models/InteractionActivityDefinition';
+import Statement from '../../../../../models/Statement';
+import SubStatementObject from '../../../../../models/SubStatementObject';
+import { statementDefaults } from './statements.fixture';
+
+const numericQuestionInteractionActivityStatement: Statement = {
+  ...statementDefaults,
+  ...{
+    result: {
+      response: '4:',
+    },
+    object: {
+      definition: {
+        name: {'en-US': 'Question 7'},
+        description: {'en-US': 'How many jokes is Chris the butt of each day?'},
+        type: 'http://adlnet.gov/expapi/activities/cmi.interaction',
+        interactionType: ActivityInteractionType.NUMERIC,
+        correctResponsesPattern: ['4:'],
+      } as Partial<InteractionActivityDefinition>,
+    } as SubStatementObject,
+  } as Partial<Statement>,
+};
+
+export {
+  numericQuestionInteractionActivityStatement,
+};
---
+++ src/apps/statements/tests/storingStatements/queriables/getMetadataFromStatement/getNumericQuestionMetadata.test.ts
@@ -0,0 +1,30 @@
+import * as assert from 'assert';
+
+import { getNumericQuestionMetadata } from '../../../../service/storeStatements/queriables/getMetadataFromStatement/getNumericQuestionMetadata';
+import { numericQuestionInteractionActivityStatement } from './fixtures/numeric-question-interaction.fixture';
+
+describe('Retrieve numeric question metadata from statement', () => {
+  it('should return empty metadata from empty result', () => {
+    const expectedEmptyMetadata = {};
+
+    const actualEmptyMetadataFromEmptyResult = getNumericQuestionMetadata(
+      {
+        ...numericQuestionInteractionActivityStatement,
+        ...{
+          result: {},
+        },
+      },
+    );
+
+    assert.deepEqual(actualEmptyMetadataFromEmptyResult, expectedEmptyMetadata);
+  });
+
+  it('should return numeric question for correct result', () => {
+    const actualCorrectMetadata = getNumericQuestionMetadata(numericQuestionInteractionActivityStatement);
+    const expectedCorrectMetadata = {
+      'https://learninglocker.net/numeric-response': 4,
+    };
+
+    assert.deepEqual(actualCorrectMetadata, expectedCorrectMetadata);
+  });
+});
