7782775abeeae7b0ee98ecc0888c973a508d193f
Vadim Nikolaenko <vadimnikolaenko.biz@gmail.com>
2020-03-02 17:15:34 +0200

    test(getStatements): Rename test variables

--- src/apps/statements/tests/getStatements/sort.test.ts
+++ src/apps/statements/tests/getStatements/sort.test.ts
@@ -7,13 +7,12 @@ import createStatement from '../utils/createStatement';
 import setup from '../utils/setup';
 import storeStatementsInService from '../utils/storeStatementsInService';
 
-const TEST_ID_1_PACK_1 = '1c86d8e9-f325-404f-b3d9-24c451035582';
-const TEST_ID_2_PACK_1 = '1c86d8e9-f325-404f-b3d9-24c451035583';
-const TEST_ID_3_PACK_1 = '1c86d8e9-f325-404f-b3d9-24c451035584';
-const TEST_ID_4_PACK_1 = '1c86d8e9-f325-404f-b3d9-24c451035585';
-
-const TEST_ID_1_PACK_2 = '1c86d8e9-f325-404f-b3d9-24c451035586';
-const TEST_ID_2_PACK_2 = '1c86d8e9-f325-404f-b3d9-24c451035587';
+const TEST_ID_1 = '1c86d8e9-f325-404f-b3d9-24c451035582';
+const TEST_ID_2 = '1c86d8e9-f325-404f-b3d9-24c451035583';
+const TEST_ID_3 = '1c86d8e9-f325-404f-b3d9-24c451035584';
+const TEST_ID_4 = '1c86d8e9-f325-404f-b3d9-24c451035585';
+const TEST_ID_5 = '1c86d8e9-f325-404f-b3d9-24c451035586';
+const TEST_ID_6 = '1c86d8e9-f325-404f-b3d9-24c451035587';
 
 const TEST_CLIENT = createClientModel();
 
@@ -28,17 +27,17 @@ describe('get statements by sorting', () => {
   const sortStatements = async (opts: GetStatementsOptions) => {
     await storeStatements(
       [
-        createStatement({ id: TEST_ID_1_PACK_1 }),
-        createStatement({ id: TEST_ID_2_PACK_1 }),
-        createStatement({ id: TEST_ID_3_PACK_1 }),
-        createStatement({ id: TEST_ID_4_PACK_1 }),
+        createStatement({ id: TEST_ID_1 }),
+        createStatement({ id: TEST_ID_2 }),
+        createStatement({ id: TEST_ID_3 }),
+        createStatement({ id: TEST_ID_4 }),
       ],
     );
     await Promise.resolve(delay(100));
     await storeStatements(
       [
-        createStatement({ id: TEST_ID_1_PACK_2 }),
-        createStatement({ id: TEST_ID_2_PACK_2 }),
+        createStatement({ id: TEST_ID_5 }),
+        createStatement({ id: TEST_ID_6 }),
       ],
     );
     const sortedStatements = await getStatements(opts);
@@ -57,12 +56,12 @@ describe('get statements by sorting', () => {
 
     const actualStatementsOrder = statements.map((s) => s.id);
     const expectedStatementsOrder = [
-      TEST_ID_1_PACK_1,
-      TEST_ID_2_PACK_1,
-      TEST_ID_3_PACK_1,
-      TEST_ID_4_PACK_1,
-      TEST_ID_1_PACK_2,
-      TEST_ID_2_PACK_2,
+      TEST_ID_1,
+      TEST_ID_2,
+      TEST_ID_3,
+      TEST_ID_4,
+      TEST_ID_5,
+      TEST_ID_6,
     ];
 
     assert.deepEqual(actualStatementsOrder, expectedStatementsOrder);
@@ -76,12 +75,12 @@ describe('get statements by sorting', () => {
 
     const actualStatementsOrder = statements.map((s) => s.id);
     const expectedStatementsOrder = [
-      TEST_ID_2_PACK_2,
-      TEST_ID_1_PACK_2,
-      TEST_ID_4_PACK_1,
-      TEST_ID_3_PACK_1,
-      TEST_ID_2_PACK_1,
-      TEST_ID_1_PACK_1,
+      TEST_ID_6,
+      TEST_ID_5,
+      TEST_ID_4,
+      TEST_ID_3,
+      TEST_ID_2,
+      TEST_ID_1,
     ];
 
     assert.deepEqual(actualStatementsOrder, expectedStatementsOrder);
