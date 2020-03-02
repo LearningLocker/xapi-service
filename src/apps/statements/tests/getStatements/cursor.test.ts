4c0eba8546cc25923d13eaf1f3da182303b040f1
Vadim Nikolaenko <vadimnikolaenko.biz@gmail.com>
2020-03-02 17:09:54 +0200

    test(getStatements): Fix tests according fixed sort logic

--- src/apps/statements/tests/getStatements/cursor.test.ts
+++ src/apps/statements/tests/getStatements/cursor.test.ts
@@ -6,69 +6,94 @@ import createStatement from '../utils/createStatement';
 import setup from '../utils/setup';
 import storeStatementsInService from '../utils/storeStatementsInService';
 
-const TEST_ID_A = '1c86d8e9-f325-404f-b3d9-24c45103558A';
-const TEST_ID_B = '1c86d8e9-f325-404f-b3d9-24c45103558B';
+const TEST_ID_1 = '1c86d8e9-f325-404f-b3d9-24c451035581';
+const TEST_ID_2 = '1c86d8e9-f325-404f-b3d9-24c451035582';
+const TEST_ID_3 = '1c86d8e9-f325-404f-b3d9-24c451035583';
+const TEST_ID_4 = '1c86d8e9-f325-404f-b3d9-24c451035584';
+
 const TEST_CLIENT = createClientModel();
 
 describe('get statements with cursors', () => {
   const service = setup();
   const storeStatements = storeStatementsInService(service);
 
+  const statement1 = createStatement({ id: TEST_ID_1 });
+  const statement2 = createStatement({ id: TEST_ID_2 });
+  const statement3 = createStatement({ id: TEST_ID_3 });
+  const statement4 = createStatement({ id: TEST_ID_4 });
+
+  beforeEach(
+    async () => {
+      await storeStatements([statement1, statement2]);
+      await Promise.resolve(delay(100));
+      await storeStatements([statement3, statement4]);
+    },
+  );
+
   it('should return correct statements when ascending cursor is used', async () => {
-    const statement1 = createStatement({ id: TEST_ID_A });
-    const statement2 = createStatement({ id: TEST_ID_B });
-    await storeStatements([statement1]);
-    await Promise.resolve(delay(10));
-    await storeStatements([statement2]);
-    const result1 = await service.getStatements({
-      limit: 1,
+    const page1Results = await service.getStatements({
+      limit: 2,
       client: TEST_CLIENT,
       ascending: true,
     });
-    assert.notEqual(result1.cursor, undefined);
-    assert(isArray(result1.statements), 'Expected an array of statements');
-    assert.deepEqual(result1.statements.map((statement) => {
-      return statement.id;
-    }), [TEST_ID_A]);
-    const result2 = await service.getStatements({
-      limit: 1,
+
+    const actualPage1StatementIdsOrder = page1Results.statements.map((s) => s.id);
+    const expectedPage1StatementIdsOrder = [
+      TEST_ID_1,
+      TEST_ID_2,
+    ];
+
+    assert.notEqual(page1Results.cursor, undefined);
+    assert(isArray(page1Results.statements), 'Expected an array of statements');
+    assert.deepEqual(actualPage1StatementIdsOrder, expectedPage1StatementIdsOrder);
+
+    const page2Results = await service.getStatements({
+      limit: 2,
       client: TEST_CLIENT,
-      cursor: result1.cursor,
+      cursor: page1Results.cursor,
       ascending: true,
     });
-    assert.equal(result2.cursor, undefined);
-    assert(isArray(result2.statements), 'Expected an array of statements');
-    assert.deepEqual(result2.statements.map((statement) => {
-      return statement.id;
-    }), [TEST_ID_B]);
+
+    const actualPage2StatementIdsOrder = page2Results.statements.map((s) => s.id);
+    const expectedPage2StatementIdsOrder = [
+      TEST_ID_3,
+      TEST_ID_4,
+    ];
+
+    assert.equal(page2Results.cursor, undefined);
+    assert(isArray(page2Results.statements), 'Expected an array of statements');
+    assert.deepEqual(actualPage2StatementIdsOrder, expectedPage2StatementIdsOrder);
   });
 
   it('should return correct statements when descending cursor is used', async () => {
-    const statement1 = createStatement({ id: TEST_ID_A });
-    const statement2 = createStatement({ id: TEST_ID_B });
-    await storeStatements([statement1]);
-    await Promise.resolve(delay(10));
-    await storeStatements([statement2]);
-    const result1 = await service.getStatements({
-      limit: 1,
+    const page1Results = await service.getStatements({
+      limit: 2,
       client: TEST_CLIENT,
       ascending: false,
     });
-    assert.notEqual(result1.cursor, undefined);
-    assert(isArray(result1.statements), 'Expected an array of statements');
-    assert.deepEqual(result1.statements.map((statement) => {
-      return statement.id;
-    }), [TEST_ID_B]);
-    const result2 = await service.getStatements({
-      limit: 1,
+    const page1ActualStatementIdsOrder = page1Results.statements.map((s) => s.id);
+    const expectedPage1StatementIdsOrder = [
+      TEST_ID_4,
+      TEST_ID_3,
+    ];
+    assert.notEqual(page1Results.cursor, undefined);
+    assert(isArray(page1Results.statements), 'Expected an array of statements');
+    assert.deepEqual(page1ActualStatementIdsOrder, expectedPage1StatementIdsOrder);
+
+    const page2Results = await service.getStatements({
+      limit: 2,
       client: TEST_CLIENT,
       ascending: false,
-      cursor: result1.cursor,
+      cursor: page1Results.cursor,
     });
-    assert.equal(result2.cursor, undefined);
-    assert(isArray(result2.statements), 'Expected an array of statements');
-    assert.deepEqual(result2.statements.map((statement) => {
-      return statement.id;
-    }), [TEST_ID_A]);
+    const page2ActualStatementIdsOrder = page2Results.statements.map((s) => s.id);
+    const page2ExpectedStatementIdsOrder = [
+      TEST_ID_2,
+      TEST_ID_1,
+    ];
+
+    assert.equal(page2Results.cursor, undefined);
+    assert(isArray(page2Results.statements), 'Expected an array of statements');
+    assert.deepEqual(page2ActualStatementIdsOrder, page2ExpectedStatementIdsOrder);
   });
 });
--- src/apps/statements/tests/getStatements/sort.test.ts
+++ src/apps/statements/tests/getStatements/sort.test.ts
@@ -1,4 +1,5 @@
 import assert from 'assert';
+import { delay } from 'bluebird';
 import { isArray } from 'lodash';
 import GetStatementsOptions from '../../serviceFactory/options/GetStatementsOptions';
 import createClientModel from '../utils/createClientModel';
@@ -6,8 +7,14 @@ import createStatement from '../utils/createStatement';
 import setup from '../utils/setup';
 import storeStatementsInService from '../utils/storeStatementsInService';
 
-const TEST_ID_1 = '1c86d8e9-f325-404f-b3d9-24c451035582';
-const TEST_ID_2 = '1c86d8e9-f325-404f-b3d9-24c451035583';
+const TEST_ID_1_PACK_1 = '1c86d8e9-f325-404f-b3d9-24c451035582';
+const TEST_ID_2_PACK_1 = '1c86d8e9-f325-404f-b3d9-24c451035583';
+const TEST_ID_3_PACK_1 = '1c86d8e9-f325-404f-b3d9-24c451035584';
+const TEST_ID_4_PACK_1 = '1c86d8e9-f325-404f-b3d9-24c451035585';
+
+const TEST_ID_1_PACK_2 = '1c86d8e9-f325-404f-b3d9-24c451035586';
+const TEST_ID_2_PACK_2 = '1c86d8e9-f325-404f-b3d9-24c451035587';
+
 const TEST_CLIENT = createClientModel();
 
 describe('get statements by sorting', () => {
@@ -19,12 +26,26 @@ describe('get statements by sorting', () => {
   };
 
   const sortStatements = async (opts: GetStatementsOptions) => {
-    await storeStatements([createStatement({ id: TEST_ID_1 })]);
-    await new Promise((resolve) => setTimeout(resolve, 1));
-    await storeStatements([createStatement({ id: TEST_ID_2 })]);
+    await storeStatements(
+      [
+        createStatement({ id: TEST_ID_1_PACK_1 }),
+        createStatement({ id: TEST_ID_2_PACK_1 }),
+        createStatement({ id: TEST_ID_3_PACK_1 }),
+        createStatement({ id: TEST_ID_4_PACK_1 }),
+      ],
+    );
+    await Promise.resolve(delay(100));
+    await storeStatements(
+      [
+        createStatement({ id: TEST_ID_1_PACK_2 }),
+        createStatement({ id: TEST_ID_2_PACK_2 }),
+      ],
+    );
     const sortedStatements = await getStatements(opts);
+
     assert(isArray(sortedStatements));
-    assert.equal(sortedStatements.length, 2);
+    assert.equal(sortedStatements.length, 6);
+
     return sortedStatements;
   };
 
@@ -33,8 +54,18 @@ describe('get statements by sorting', () => {
       ascending: true,
       client: TEST_CLIENT,
     });
-    assert.equal(statements[0].id, TEST_ID_1);
-    assert.equal(statements[1].id, TEST_ID_2);
+
+    const actualStatementsOrder = statements.map((s) => s.id);
+    const expectedStatementsOrder = [
+      TEST_ID_1_PACK_1,
+      TEST_ID_2_PACK_1,
+      TEST_ID_3_PACK_1,
+      TEST_ID_4_PACK_1,
+      TEST_ID_1_PACK_2,
+      TEST_ID_2_PACK_2,
+    ];
+
+    assert.deepEqual(actualStatementsOrder, expectedStatementsOrder);
   });
 
   it('should return statements in the correct order when ascending', async () => {
@@ -42,7 +73,17 @@ describe('get statements by sorting', () => {
       ascending: false,
       client: TEST_CLIENT,
     });
-    assert.equal(statements[1].id, TEST_ID_1);
-    assert.equal(statements[0].id, TEST_ID_2);
+
+    const actualStatementsOrder = statements.map((s) => s.id);
+    const expectedStatementsOrder = [
+      TEST_ID_2_PACK_2,
+      TEST_ID_1_PACK_2,
+      TEST_ID_4_PACK_1,
+      TEST_ID_3_PACK_1,
+      TEST_ID_2_PACK_1,
+      TEST_ID_1_PACK_1,
+    ];
+
+    assert.deepEqual(actualStatementsOrder, expectedStatementsOrder);
   });
 });
