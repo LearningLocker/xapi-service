68b10f9bc62b4731a1df9c6bf23f3bb70fb2f6b0
Yevhen <yevhen.sharpinskyi@gmail.com>
2020-03-09 16:34:12 +0200

    feat(Metadata): Add parsed boolean response. (https://github.com/LearningLocker/xapi-service/pull/591- [LL-256](https://learningpool.atlassian.net/browse/LL-256))
    https://github.com/LearningLocker/xapi-service/pull/591
    https://learningpool.atlassian.net/browse/LL-256

--- src/apps/statements/service/storeStatements/queriables/getMetadataFromStatement/index.ts
+++ src/apps/statements/service/storeStatements/queriables/getMetadataFromStatement/index.ts
@@ -1,13 +1,16 @@
 import Statement from '../../../../models/Statement';
+import { getBooleanMetadata } from './getBooleanMetadata';
 import { getDurationMetadata } from './getDurationMetadata';
 import { getSequencingMetadata } from './getSequencingMetadata';
 
-export default (statement: Statement): {readonly [key: string]: any} => {
+export default (statement: Statement): { readonly [key: string]: any } => {
   const durationMetadata = getDurationMetadata(statement);
   const sequencingMetadata = getSequencingMetadata(statement);
+  const booleanMetadata = getBooleanMetadata(statement);
 
   return {
     ...durationMetadata,
     ...sequencingMetadata,
+    ...booleanMetadata,
   };
 };
