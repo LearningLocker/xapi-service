59d2da487df8df9af6132c2da6ae64fb90dcaf17
Vadim <vadimnikolaenko.biz@gmail.com>
2020-03-09 17:05:53 +0200

    feat(Metadata): Add parsed choice response (https://github.com/LearningLocker/xapi-service/pull/583 - [LL-258](https://learningpool.atlassian.net/browse/LL-258))
    https://learningpool.atlassian.net/browse/LL-258
    https://github.com/LearningLocker/xapi-service/pull/583

--- src/apps/statements/service/storeStatements/queriables/getMetadataFromStatement/index.ts
+++ src/apps/statements/service/storeStatements/queriables/getMetadataFromStatement/index.ts
@@ -1,5 +1,6 @@
 import Statement from '../../../../models/Statement';
 import { getBooleanMetadata } from './getBooleanMetadata';
+import { getChoiceQuestionMetadata } from './getChoiceQuestionMetadata';
 import { getDurationMetadata } from './getDurationMetadata';
 import { getMatchingQuestionsMetadata } from './getMatchingQuestionsMetadata';
 import { getSequencingMetadata } from './getSequencingMetadata';
@@ -7,12 +8,14 @@ import { getSequencingMetadata } from './getSequencingMetadata';
 export default (statement: Statement): { readonly [key: string]: any } => {
   const durationMetadata = getDurationMetadata(statement);
   const sequencingMetadata = getSequencingMetadata(statement);
+  const choicesMetadata = getChoiceQuestionMetadata(statement);
   const matchingMetadata = getMatchingQuestionsMetadata(statement);
   const booleanMetadata = getBooleanMetadata(statement);
 
   return {
     ...durationMetadata,
     ...sequencingMetadata,
+    ...choicesMetadata,
     ...matchingMetadata,
     ...booleanMetadata,
   };
