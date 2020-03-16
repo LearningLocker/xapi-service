708e6cffc7c048a9c3ac3dc0daafc1f55a9d7f76
Vadim Nikolaenko <vadimnikolaenko.biz@gmail.com>
2020-03-16 14:28:23 +0200

    test(getStatements): Remove istanbul ignore comment

--- src/apps/statements/tests/getStatements/utils/assertStatementsPageResultsAndOrder.ts
+++ src/apps/statements/tests/getStatements/utils/assertStatementsPageResultsAndOrder.ts
@@ -4,7 +4,6 @@ import { isArray, isString } from 'lodash';
 import ClientModel from '../../../models/ClientModel';
 import Service from '../../../serviceFactory/Service';
 
-/* istanbul ignore next */
 export const assertStatementsPageResultsAndOrder = async (
   {
     service,
