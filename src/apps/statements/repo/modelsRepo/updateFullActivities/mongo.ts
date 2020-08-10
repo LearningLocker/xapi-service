9d665d64dc699c51f0fc6d3f505094e30d6505c8
Eugene Sharpinskyi <yevhen.sharpinskyi@gmail.com>
2020-08-10 17:25:13 +0300

    fix(API): Fix merging contextActivities Merging contextActivities instead of rewriting

--- src/apps/statements/repo/modelsRepo/updateFullActivities/mongo.ts
+++ src/apps/statements/repo/modelsRepo/updateFullActivities/mongo.ts
@@ -1,5 +1,6 @@
-import { Dictionary, isEmpty, mapKeys } from 'lodash';
-import { ObjectID } from 'mongodb';
+import { Dictionary, mapKeys } from 'lodash';
+import { ObjectID, UnorderedBulkOperation } from 'mongodb';
+import FullActivityDatabase from '../../../models/FullActivityDatabase';
 import { FULL_ACTIVITIES_COLLECTION_NAME } from '../utils/mongoModels/constants';
 import FacadeConfig from '../utils/mongoModels/FacadeConfig';
 import matchesFullActivity from '../utils/mongoModels/matchesFullActivity';
@@ -12,47 +13,98 @@ const getPatchUpdate = <T>(patch: Dictionary<T>, parentKeys: string[]) =>
     (_value: T, key: number) => `${parentKeys.join('.')}.${key}`,
   );
 
+const createMongoAddToSet = (fullActivity: FullActivityDatabase) => (
+  {
+    ...(fullActivity.context?.contextActivities?.parent !== undefined
+        ? {
+          'context.contextActivities.parent': {
+            $each: fullActivity.context.contextActivities.parent,
+          },
+        }
+        : {}
+    ),
+    ...(fullActivity.context?.contextActivities?.category !== undefined
+        ? {
+          'context.contextActivities.category': {
+            $each: fullActivity.context.contextActivities.category,
+          },
+        }
+        : {}
+    ),
+    ...(fullActivity.context?.contextActivities?.other !== undefined
+        ? {
+          'context.contextActivities.other': {
+            $each: fullActivity.context.contextActivities.other,
+          },
+        }
+        : {}
+    ),
+    ...(fullActivity.context?.contextActivities?.grouping !== undefined
+        ? {
+          'context.contextActivities.grouping': {
+            $each: fullActivity.context.contextActivities.grouping,
+          },
+        }
+        : {}
+    ),
+  }
+);
+
+const createMongoSet = (fullActivity: FullActivityDatabase) => {
+  const extensions = replaceDotsInExtensions(/\./g, '&46;')(fullActivity.extensions);
+
+  return {
+    ...getPatchUpdate(fullActivity.name, ['name']),
+    ...getPatchUpdate(fullActivity.description, ['description']),
+    ...getPatchUpdate(extensions, ['extensions']),
+    ...(
+      fullActivity.moreInfo !== undefined
+        ? { moreInfo: fullActivity.moreInfo }
+        : {}
+    ),
+    ...(
+      fullActivity.type !== undefined
+        ? { type: fullActivity.type }
+        : {}
+    ),
+  };
+};
+
+const createMongoQuery = (fullActivity: FullActivityDatabase) => {
+  const activityId = fullActivity.activityId;
+  const lrsId = new ObjectID(fullActivity.lrsId);
+  const organisationId = new ObjectID(fullActivity.organisationId);
+
+  return matchesFullActivity({ activityId, lrsId, organisationId });
+};
+
+const creatBatchQuery = (batch: UnorderedBulkOperation) => (fullActivity: FullActivityDatabase) => {
+  const mongoQuery = createMongoQuery(fullActivity);
+  const mongoSet = createMongoSet(fullActivity);
+  const mongoAddToSet = createMongoAddToSet(fullActivity);
+
+  if (Object.keys(mongoSet).length === 0 && Object.keys(mongoAddToSet).length === 0) {
+    return;
+  }
+
+  batch.find(mongoQuery).upsert().updateOne(
+    {
+      $set: mongoSet,
+      $addToSet: mongoAddToSet,
+    },
+  );
+};
+
 export default (config: FacadeConfig): Signature =>
   async ({ fullActivities }) => {
+    if (fullActivities.length < 1) {
+      return;
+    }
+
     const collection = (await config.db()).collection(FULL_ACTIVITIES_COLLECTION_NAME);
     const batch = collection.initializeUnorderedBulkOp();
 
-    fullActivities.forEach((fullActivity) => {
-      const activityId = fullActivity.activityId;
-      const lrsId = new ObjectID(fullActivity.lrsId);
-      const organisationId = new ObjectID(fullActivity.organisationId);
-      const mongoQuery = matchesFullActivity({ activityId, lrsId, organisationId });
-      const extensions = replaceDotsInExtensions(/\./g, '&46;')(fullActivity.extensions);
-      const mongoSet = {
-        ...getPatchUpdate(fullActivity.name, ['name']),
-        ...getPatchUpdate(fullActivity.description, ['description']),
-        ...getPatchUpdate(extensions, ['extensions']),
-        ...(
-          !isEmpty(fullActivity.context)
-            ? { context: fullActivity.context }
-            : {}
-        ),
-        ...(
-          fullActivity.moreInfo !== undefined
-            ? { moreInfo: fullActivity.moreInfo }
-            : {}
-        ),
-        ...(
-          fullActivity.type !== undefined
-            ? { type: fullActivity.type }
-            : {}
-        ),
-      };
-
-      if (Object.keys(mongoSet).length === 0) {
-        /* istanbul ignore next */
-        return;
-      }
-
-      batch.find(mongoQuery).upsert().updateOne({ $set: mongoSet });
-    });
-
-    if (fullActivities.length > 0) {
-      await batch.execute();
-    }
+    fullActivities.forEach(creatBatchQuery(batch));
+    await batch.execute();
+    // tslint:disable-next-line:max-file-line-count
   };
