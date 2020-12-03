34bc82f11063eebf6ad4bd65365dc6587619d27e
Eugene Sharpinskyi <yevhen.sharpinskyi@gmail.com>
2020-12-03 11:53:09 +0200

    feat(API): Add organisationId to redis message

--- src/apps/statements/repo/eventsRepo/emitNewStatements/redis.ts
+++ src/apps/statements/repo/eventsRepo/emitNewStatements/redis.ts
@@ -5,11 +5,11 @@ const EVENT_NAME = 'statement.new';
 const CHANNEL_NAME = 'statement.notify';
 
 export default (config: FacadeConfig): Signature => {
-  return async ({ ids }) => {
+  return async ({ statementProperties }) => {
     const client = await config.client();
     const listName = `${config.prefix}:${EVENT_NAME}`;
     const channelName = `${config.prefix}:${CHANNEL_NAME}`;
-    await client.rpush(listName, ...ids);
+    await client.rpush(listName, ...statementProperties);
     client.publish(channelName, '');
   };
 };
