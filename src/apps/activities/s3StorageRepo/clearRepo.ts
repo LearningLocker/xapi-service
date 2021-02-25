cadd10a57624f64590e12ff39e875cdff5aa3e2c
Ryan Smith <0ryansmith1994@gmail.com>
2021-02-25 18:16:50 +0000

    ci: Attempts to debug flaky test

--- src/apps/activities/s3StorageRepo/clearRepo.ts
+++ src/apps/activities/s3StorageRepo/clearRepo.ts
@@ -6,34 +6,39 @@ export default (config: Config) => {
     const debug = Math.random();
     // Gets all of the objects to be deleted.
     console.debug('101', debug); // tslint:disable-line: no-console - 2021-02-25 flaky CI
-    const listObjectsOutput = await config.client.listObjects({
-      Bucket: config.bucketName,
-      Prefix: config.subFolder,
-    }).promise().catch((err) => {
-      console.debug('501', debug, err); // tslint:disable-line: no-console - 2021-02-25 flaky client
-      throw err;
-    });
-    console.debug('102', debug); // tslint:disable-line: no-console - 2021-02-25 flaky CI
-    const objects = listObjectsOutput.Contents !== undefined ? listObjectsOutput.Contents : [];
-    const identifierList: S3.ObjectIdentifierList = objects.reduce(
-      (identifiers, { Key }) => {
-        if (Key !== undefined) {
-          return [...identifiers, { Key }];
-        }
-        return identifiers;
-      },
-      [] as S3.ObjectIdentifierList,
-    );
+    try {
+      const listObjectsOutput = await config.client.listObjects({
+        Bucket: config.bucketName,
+        Prefix: config.subFolder,
+      }).promise().catch((err) => {
+        console.debug('501', debug, err); // tslint:disable-line: no-console - 2021-02-25 flaky CI
+        throw err;
+      });
+      console.debug('102', debug); // tslint:disable-line: no-console - 2021-02-25 flaky CI
+      const objects = listObjectsOutput.Contents !== undefined ? listObjectsOutput.Contents : [];
+      const identifierList: S3.ObjectIdentifierList = objects.reduce(
+        (identifiers, { Key }) => {
+          if (Key !== undefined) {
+            return [...identifiers, { Key }];
+          }
+          return identifiers;
+        },
+        [] as S3.ObjectIdentifierList,
+      );
 
-    // Deletes the objects.
-    if (identifierList.length === 0) {
-      return;
+      // Deletes the objects.
+      if (identifierList.length === 0) {
+        return;
+      }
+      console.debug('103', debug); // tslint:disable-line: no-console - 2021-02-25 flaky CI
+      await config.client.deleteObjects({
+        Bucket: config.bucketName,
+        Delete: { Objects: identifierList },
+      }).promise();
+      console.debug('104', debug); // tslint:disable-line: no-console - 2021-02-25 flaky CI
+    } catch (err) {
+      console.debug('502', debug, err); // tslint:disable-line: no-console - 2021-02-25 flaky CI
+      throw err;
     }
-    console.debug('103', debug); // tslint:disable-line: no-console - 2021-02-25 flaky CI
-    await config.client.deleteObjects({
-      Bucket: config.bucketName,
-      Delete: { Objects: identifierList },
-    }).promise();
-    console.debug('104', debug); // tslint:disable-line: no-console - 2021-02-25 flaky CI
   };
 };
