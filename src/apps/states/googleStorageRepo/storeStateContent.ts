9eff3bd7af1e6783c54bf2d947d896606fc7fa9a
Ryan Smith <0ryansmith1994@gmail.com>
2021-03-04 12:09:17 +0000

    fix: Attempts to fix flaky Azure & Google tests

--- src/apps/states/googleStorageRepo/storeStateContent.ts
+++ src/apps/states/googleStorageRepo/storeStateContent.ts
@@ -4,15 +4,25 @@ import Config from './Config';
 
 export default (config: Config) => {
   return async (opts: StoreStateContentOptions): Promise<void> => {
-    return new Promise<void>((resolve, reject) => {
-      const dir = getStorageDir({ subfolder: config.subFolder, lrs_id: opts.lrs_id });
-      const filePath = `${dir}/${opts.key}`;
-      const file = config.storage.bucket(config.bucketName).file(filePath);
-      const writeStream = file.createWriteStream();
-      opts.content.pipe(writeStream);
-      writeStream.on('finish', resolve);
+    const dir = getStorageDir({ subfolder: config.subFolder, lrs_id: opts.lrs_id });
+    const filePath = `${dir}/${opts.key}`;
+    const file = config.storage.bucket(config.bucketName).file(filePath);
+    const writeStream = file.createWriteStream();
+
+    const contentStreamPromise = new Promise<void>((resolve, reject) => {
       opts.content.on('error', reject);
+      opts.content.on('end', resolve);
+      opts.content.on('close', resolve);
+      opts.content.on('finish', resolve);
+    });
+
+    const writeStreamPromise = new Promise<void>((resolve, reject) => {
+      writeStream.on('finish', resolve);
       writeStream.on('error', reject);
     });
+
+    opts.content.pipe(writeStream);
+
+    await Promise.all([contentStreamPromise, writeStreamPromise]);
   };
 };
