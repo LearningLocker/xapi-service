8c7724dd8c266d11f07433513d8c73f4fe4b412c
Eugene Sharpinskyi <yevhen.sharpinskyi@gmail.com>
2021-12-13 16:58:06 +0200

    feat(Redis): Create the configuration object for Redis

--- src/utils/connectToRedis.ts
+++ src/utils/connectToRedis.ts
@@ -1,11 +1,27 @@
+import fs from 'fs';
 import Ioredis from 'ioredis';
-import { defaultTo, once } from 'lodash';
+import { once } from 'lodash';
 import config from '../config';
 import logger from '../logger';
 
 export default once((): (() => Promise<Ioredis.Redis>) => {
   return once(async () => {
     logger.info('Creating redis connection');
-    return new Ioredis(defaultTo(config.redis.url, 'redis://127.0.0.1:6379/0'));
+
+    if (config.redis.tlsIsEnabled) {
+      return new Ioredis({
+        db: config.redis.db,
+        password: config.redis.password,
+        host: config.redis.host,
+        port: config.redis.port,
+        tls: {
+          cert: fs.readFileSync('./certificates/redis/redis.crt'),
+          key: fs.readFileSync('./certificates/redis/redis.key'),
+          ca: fs.readFileSync('./certificates/redis/ca.crt'),
+        },
+      });
+    }
+
+    return new Ioredis(config.redis.url);
   });
 });
