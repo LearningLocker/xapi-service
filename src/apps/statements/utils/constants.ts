284c9388777c4564bb1a12623e28648b4ca64dcd
Vadim Nikolaenko <vadimnikolaenko.biz@gmail.com>
2022-01-28 14:40:52 +0200

    feat(Statements app): Add jsonContentType to constants.ts

--- src/apps/statements/utils/constants.ts
+++ src/apps/statements/utils/constants.ts
@@ -1,5 +1,6 @@
 export const xapiStatementVersion = '1.0.0';
 export const xapiHeaderVersion = '1.0.3';
 export const statementsRoute = '/xAPI/statements';
+export const jsonContentType = 'application/json';
 export const aboutRoute = '/xAPI/about';
 export const fullActivitiesRoute = '/xAPI/activities';
