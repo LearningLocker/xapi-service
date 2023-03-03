06674affb9892541b078e170f7ff60843a6838b4
Eugene Sharpinskyi <yevhen.sharpinskyi@gmail.com>
2023-03-03 10:30:45 +0200

    feat(Statement): Publish to SQS instead of Redis

---
+++ src/apps/statements/repo/eventsRepo/emitNewStatements/sqs.ts
@@ -0,0 +1,80 @@
+import {
+  GetQueueUrlCommand,
+  SQSClient,
+  SendMessageBatchCommand,
+  SendMessageBatchRequestEntry,
+} from '@aws-sdk/client-sqs';
+import { v4 } from 'uuid';
+import { getPrefixWithProcessingPriority } from '../utils/getPrefixWithProcessingPriority';
+import { StatementProcessingPriority } from '../../../enums/statementProcessingPriority.enum';
+import FacadeConfig from '../utils/sqsEvents/FacadeConfig';
+import { STATEMENT_QUEUE } from '../utils/constants';
+import logger from '../../../../../logger';
+import Signature from './Signature';
+
+const MAX_BATCH_SIZE = 10;
+
+let queueUrl: string | undefined;
+
+const publishMessages = async (sqsClient: SQSClient, statementProperties: string[]) => {
+  const statementPropertiesBatchRequest = statementProperties.map(
+    (statementProperty): SendMessageBatchRequestEntry => ({
+      Id: v4(),
+      MessageBody: statementProperty,
+    }),
+  );
+
+  for (let index = 0; index < statementPropertiesBatchRequest.length; index += MAX_BATCH_SIZE) {
+    await sqsClient.send(
+      new SendMessageBatchCommand({
+        QueueUrl: queueUrl,
+        Entries: statementPropertiesBatchRequest.slice(index, index + MAX_BATCH_SIZE),
+      }),
+    );
+  }
+};
+
+const getQueueUrl = async (
+  sqsClient: SQSClient,
+  prefix: string,
+  priority: StatementProcessingPriority,
+  isQueuePriorityEnabled: boolean,
+) => {
+  if (queueUrl) {
+    logger.info('queueUrl from CACHE');
+    return queueUrl;
+  }
+
+  const prefixWithPriority = getPrefixWithProcessingPriority(
+    prefix,
+    priority,
+    isQueuePriorityEnabled,
+  );
+
+  const getQueueUrlCommand = new GetQueueUrlCommand({
+    QueueName: `${prefixWithPriority}_${STATEMENT_QUEUE}`,
+  });
+
+  const commandResult = await sqsClient.send(getQueueUrlCommand);
+  logger.info('queueUrl from QUERY');
+
+  queueUrl = commandResult.QueueUrl;
+
+  return queueUrl;
+};
+
+export default (config: FacadeConfig): Signature => {
+  return async ({ statementProperties, priority }) => {
+    const sqsClient = await config.client();
+    const queueUrl = await getQueueUrl(
+      sqsClient,
+      config.prefix,
+      priority,
+      config.isQueuePriorityEnabled,
+    );
+
+    logger.info('QUEUE_URL --->', queueUrl);
+
+    await publishMessages(sqsClient, statementProperties);
+  };
+};
--- src/apps/statements/repo/eventsRepo/utils/constants.ts
+++ src/apps/statements/repo/eventsRepo/utils/constants.ts
@@ -1,2 +1,3 @@
 export const EVENT_NAME = 'statement.new';
 export const CHANNEL_NAME = 'statement.notify';
+export const STATEMENT_QUEUE = 'STATEMENT_QUEUE';
